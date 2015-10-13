fa23762752bdded040d48da1fe93a88d47cf823e
Jonathan Young <jon.young@utexas.edu>
2015-10-13 16:15:01 -0500

    Corrections to assembling network

--- src/clust_intact.py
+++ src/clust_intact.py
@@ -27,7 +27,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import os
-import pickle
 import pyuserfcn
 import scipy.special
 import scipy.stats as stats
@@ -39,11 +38,11 @@ from sklearn import metrics
 
 def process_func_net(netwkFile):
     """Get nodes and edge weights of functional net and store in dictionary
-    RETURNS:
-        <dict> {(gene ID1, gene ID2): LLS}
-    """
+    RETURNS <dict> {(gene ID1, gene ID2): LLS}"""
     node2edgewt = dict()
     netwkDir = os.path.join('..', '..', 'DataDownload', 'FunctionalNet', '')
+    if not os.path.exists(netwkDir + netwkFile):
+        netwkDir = os.path.join('..', 'data', '')
     scoreCol = 2
     for line in open(netwkDir + netwkFile):
         tokens = line.split('\t')
@@ -53,11 +52,8 @@ def process_func_net(netwkFile):
 
 def assign_gene_indices(node2edgewt):
     """Assign indices to genes to facilitate adjacency matrix creation
-    ARGUMENTS:
-        <dict> {(gene node 1, gene node 2): LLS}
-    RETURNS:
-        <dict> {gene: matrix index}
-    """
+    ARGUMENTS: <dict> {(gene node 1, gene node 2): LLS}
+    RETURNS <dict> {gene: matrix index}"""
     allGenes = set()
     for vx in node2edgewt.keys():
         allGenes.update(vx)
@@ -68,11 +64,9 @@ def assign_gene_indices(node2edgewt):
 
 def build_netwk_adj_matrix(node2edgewt, gene2idx):
     """ARGUMENTS:
-        <dict> {(gene node 1, gene node 2): LLS}
-        <dict> {gene: matrix index}
-    RETURNS:
-        <ndarray> adjacency matrix
-    """
+        1.) <dict> {(gene node 1, gene node 2): LLS}
+        2.) <dict> {gene: matrix index}
+    RETURNS <ndarray> adjacency matrix"""
     adjMat = np.zeros((len(gene2idx), len(gene2idx)))
     for vx in node2edgewt.keys():
         adjMat[gene2idx[vx[0]], gene2idx[vx[1]]] = node2edgewt[vx]
@@ -85,11 +79,10 @@ def read_biogrid(experimentSys, filename):
     interactor ID to its genetic interaction partners
     NOTE: the correct column numbers need to be specified beforehand: 1 & 2 
     for Entrez, 5 & 6 for systematic name, 7 & 8 for official symbol
-    ARGUMENTS:
-        <string> type of genetic interaction
-    RETURNS:
-        <dict> {seed Entrez: {interactors' Entrez}}
-    """
+    ARGUMENTS: 
+        1.) <string> type of genetic interaction
+        2.) <string> BIOGRID file name
+    RETURNS <dict> {seed Entrez: {interactors' Entrez}}"""
     seedSets = collections.defaultdict(set)
     allGenes = set()
     fileDir = os.path.join('..', 'data', '')
@@ -97,9 +90,9 @@ def read_biogrid(experimentSys, filename):
     for line in open(fileDir + filename):
         tokens = line.split('\t')
         if tokens[experimentalSysColNum] == experimentSys:
-            seedSets[tokens[7]].add(tokens[8])
-            seedSets[tokens[8]].add(tokens[7])
-            allGenes.update([tokens[7], tokens[8]])
+            seedSets[tokens[1]].add(tokens[2])
+            seedSets[tokens[2]].add(tokens[1])
+            allGenes.update([tokens[1], tokens[2]])
     return seedSets
 
 
@@ -141,8 +134,7 @@ def get_predictive_seeds(seedAUC, lowerLim, upperLim):
         1.) <list> [(seed AUC, seed gene)] ascend sort
         2.) <float> minimum AUC of seed set
         3.) <float> maximum AUC of seed set
-    RETURNS: <list> [genes] ascend sort by AUC
-    """
+    RETURNS <list> [genes] ascend sort by AUC"""
     predictiveSeeds = list()
     for p in seedAUC:  # p=(AUC, gene)
         if p[0] >= lowerLim and p[0] < upperLim:
@@ -154,7 +146,7 @@ def get_predictive_seeds(seedAUC, lowerLim, upperLim):
 def jaccard(s1, s2):
     """Calculate Jaccard index between 2 sets
     INPUT: 1.) & 2.) <set>
-    RETURN: <float>"""
+    RETURN <float>"""
     if isinstance(s1, set) and isinstance(s2, set):
         return len(s1 & s2)/len(s1 | s2)
     elif isinstance(s1, frozenset) and isinstance(s2, frozenset):
@@ -167,7 +159,7 @@ def jaccard(s1, s2):
 def combine_sets(setOfSets):
     """Combine various sets if they have enough in common measured by Jaccard
     INPUT & OUTPUT: <set> elements of set are frozensets"""
-    minJaccard = 0.8
+    minJaccard = 0.5
     print('The minimum Jaccard index is', minJaccard, '\n')
     # the following 17 lines are the "do" part of a "do-while" loop
     newSetOfSets = set()  # DO START
@@ -224,7 +216,7 @@ def get_interacting_pairs(seeds, seed2interactors):
     INPUT:
         1.) <iterable> seed genes
         2.) <dict> {seed gene: [interactor genes]}
-    RETURNS: <set> {(seed, interactor), (interactor, seed)}"""
+    RETURNS <set> {(seed, interactor), (interactor, seed)}"""
     interactPairs = set()
     for seed in seeds:
         for interactor in seed2interactors[seed]:
@@ -237,81 +229,92 @@ def interaction_stats(interactPairs, id2set, gene2idx, adjMat):
     INPUT:
         1.) <set> {(seed, interactor), (interactor, seed)}
         2.) <dict> {int: frozensets}
-    RETURN <dict> {(int,int):(# in 1st set, # in 2nd set, # interact, p-val)}"""
+        3.) <dict> {gene: matrix index}
+        4.) <ndarray> adjacency matrix
+    RETURN <dict> {(int,int): p-val}"""
     numGenes = len(set(itertools.chain.from_iterable(interactPairs)))
     p = (len(interactPairs)/2)/scipy.special.binom(numGenes, 2)
-    results = dict()
+    pvals = dict()
     for idPair in itertools.combinations(id2set.keys(), 2):
         set1st = id2set[idPair[0]]
         set2nd = id2set[idPair[1]]
-        #--- BEGIN NEW
-        list1st = list(set1st - set2nd)
-        list2nd = list(set2nd - set1st)
+        
+        # remove intersection & genes w/o functional partners
+        set1 = set1st - set2nd
+        set2 = set2nd - set1st
         deleteSet = set()
-        for i in range(len(list1st) - 1):
-            gene = list1st[i]
-            partners = [gene2idx[g] for g in list1st[i+1:]]
-            if np.sum(adjMat[gene2idx[gene], partners], axis=0) == 0:
-                deleteSet.add(gene)
-        set1st = {g for g in list1st if g not in deleteSet}
+        idx1 = [gene2idx[g] for g in set1]
+        for g in set1:
+            if np.sum(adjMat[gene2idx[g], idx1], axis=0) == 0:
+                deleteSet.add(g)
+        set1st = {g for g in set1 if g not in deleteSet}
         deleteSet = set()
-        for i in range(len(list2nd) - 1):
-            gene = list2nd[i]
-            partners = [gene2idx[g] for g in list2nd[i+1:]]
-            if np.sum(adjMat[gene2idx[gene], partners], axis=0) == 0:
-                deleteSet.add(gene)
-        set2nd = {g for g in list2nd if g not in deleteSet}
-        #--- END NEW
+        idx2 = [gene2idx[g] for g in set2]
+        for g in set2:
+            if np.sum(adjMat[gene2idx[g], idx2], axis=0) == 0:
+                deleteSet.add(g)
+        set2nd = {g for g in set2 if g not in deleteSet}
+        
         count = 0
         num1stSet, num2ndSet = len(set1st), len(set2nd)
         for genePair in itertools.product(set1st, set2nd):
             if genePair in interactPairs:
                 count += 1
         n = num1stSet * num2ndSet
-        pval = stats.binom.pmf(count, n, p) + stats.binom.sf(count, n, p)
-        results[idPair] = (num1stSet, num2ndSet, count, pval)
-    return results
+        pvals[idPair] = stats.binom.pmf(count,n,p) + stats.binom.sf(count,n,p)
+    return pvals
 
 
-def get_graph_edges(node2edgewt, pvalCutoff, results, id2set, intactPairs, gene2idx, adjMat):
-    """Write network input file for visualization
+def net_edges(node2edgewt, pair2pval, id2set, intactPairs, gene2idx, adjMat):
+    """Get network edges
     INPUTS:
         1.) <dict> {(gene ID1, gene ID2): LLS}
-        2.) <float> p-value cutoff
-        3.) <dict> {(int,int):(# in 1st set, # in 2nd set, # interact, p-val)}
-        4.) <dict> {int: frozensets}
-        5.) <set> {(seed, interactor), (interactor, seed)}"""
+        2.) <dict> {(int,int): p-val}
+        3.) <dict> {int: frozensets}
+        4.) <set> {(seed, interactor), (interactor, seed)}
+        5.) <dict> {gene: matrix index}
+        6.) <ndarray> adjacency matrix
+    RETURN <list> [(node, node, edge weight)]"""
+    # multiple hypothesis correction
+    pvals = np.array(list(pair2pval.values()))
+    rejected, pvalsCor = fdrcorrection0(pvals)
+    numSig = np.sum(rejected)
+    print('Number of significant interacting pairs (5% FDR):', numSig, '\n')
+    pvalsSig = np.sort(pvals[rejected])
+    numExam = 1
+    if numExam == 'all':
+        pvalCutoff = pvalsSig[pvalsSig.size - 1]
+    else:
+        pvalCutoff = pvalsSig[numExam - 1]
+    
     edges = list()
-    for idPair in results.keys():
-        if results[idPair][3] <= pvalCutoff:
+    for idPair in pair2pval.keys():
+        if pair2pval[idPair] <= pvalCutoff:
             set1st = id2set[idPair[0]]
             set2nd = id2set[idPair[1]]
-            print('Genes in 1st set:')  # CHECKING
-            print(set1st)  # CHECKING
-            print('Genes in 2nd set:')  # CHECKING
-            print(set2nd)  # CHECKING
-            print()  # CHECKING
-            #--- BEGIN NEW
-            list1st = list(set1st - set2nd)
-            list2nd = list(set2nd - set1st)
+            
+            # remove intersection & genes w/o functional partners
+            set1 = set1st - set2nd
+            set2 = set2nd - set1st
             deleteSet = set()
-            for i in range(len(list1st) - 1):
-                gene = list1st[i]
-                partners = [gene2idx[g] for g in list1st[i+1:]]
-                if np.sum(adjMat[gene2idx[gene], partners], axis=0) == 0:
-                    deleteSet.add(gene)
-            set1st = {g for g in list1st if g not in deleteSet}
+            idx1 = [gene2idx[g] for g in set1]
+            for g in set1:
+                if np.sum(adjMat[gene2idx[g], idx1], axis=0) == 0:
+                    deleteSet.add(g)
+            set1st = {g for g in set1 if g not in deleteSet}
             deleteSet = set()
-            for i in range(len(list2nd) - 1):
-                gene = list2nd[i]
-                partners = [gene2idx[g] for g in list2nd[i+1:]]
-                if np.sum(adjMat[gene2idx[gene], partners], axis=0) == 0:
-                    deleteSet.add(gene)
-            set2nd = {g for g in list2nd if g not in deleteSet}
-            #--- END NEW
+            idx2 = [gene2idx[g] for g in set2]
+            for g in set2:
+                if np.sum(adjMat[gene2idx[g], idx2], axis=0) == 0:
+                    deleteSet.add(g)
+            set2nd = {g for g in set2 if g not in deleteSet}
+            
+            # get interaction edges
             for genePair in itertools.product(set1st, set2nd):
                 if genePair in intactPairs:
                     edges.append((genePair[0], genePair[1], -1))
+            
+            # get functional edges
             for genePair in itertools.combinations(set1st, 2):
                 if genePair in node2edgewt:
                     edges.append((genePair[0], genePair[1], node2edgewt[genePair]))
@@ -352,6 +355,7 @@ def plot_network(edges, experimentSys, organism, saveFlag=0):
         writeFilename = organism + ''.join(experimentSys.split(' ')) + 'Net.svg'
         plt.savefig(writeDir+writeFilename)
     
+    plt.axis('off')
     plt.show()
 
 
@@ -384,7 +388,10 @@ def main():
                            'BIOGRID-3.4.127-human.txt'),
                  'yeast': ('yeastnet2.gene.txt', 
                            'YeastNet2_adj_matrix.npy', 
-                           'BIOGRID-3.4.127-for-yeastnetv2.txt')}
+                           'BIOGRID-3.4.127-for-yeastnetv2.txt')
+                 'fly': ('FlyNetEntrez-noNull.txt',
+                         'FlyNet_adj_matrix.npy',
+                         'BIOGRID-3.4.127-fly.txt')}
     if organism not in org2files:
         print('Organism not found.')
         print('Available organisms: human, yeast')
@@ -415,23 +422,13 @@ def main():
     for seed in predictiveSeeds:
         setOfSets.add( frozenset(set(seed2intacts[seed])) )
     combinedSets = combine_sets(setOfSets)
+
     id2set = {i: fs for i,fs in enumerate(combinedSets)}
 
-    results = interaction_stats(intactPairs, id2set, gene2idx, adjMat)
-    print('Number of set pairs:', len(results), '\n')
+    pvals = interaction_stats(intactPairs, id2set, gene2idx, adjMat)
+    print('Number of set pairs:', len(pvals), '\n')
 
-    pvals = np.array([results[k][3] for k in results.keys()])
-    rejected, pvalsCor = fdrcorrection0(pvals)
-    numSig = np.sum(rejected)
-    print('Number of significant interacting pairs (5% FDR):', numSig, '\n')
-    pvalsSig = np.sort(pvals[rejected])
-    numExam = 1
-    if numExam == 'all':
-        pvalCutoff = pvalsSig[pvalsSig.size - 1]
-    else:
-        pvalCutoff = pvalsSig[numExam - 1]
-
-    edges = get_graph_edges(node2edgewt, pvalCutoff, results, id2set, intactPairs, gene2idx, adjMat)
+    edges = net_edges(node2edgewt, pvals, id2set, intactPairs, gene2idx, adjMat)
     plot_network(edges, experimentSys, organism)
 
     writeToJSON = 0
