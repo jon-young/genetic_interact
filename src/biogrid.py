f6bbc4d3edbbacb3911ce31e1ef24e65dae0c185
Jonathan Young <jon.young@utexas.edu>
2016-01-11 18:43:33 -0600

    Deleted redundant function; column number no longer hard-coded

--- src/biogrid.py
+++ src/biogrid.py
@@ -1,59 +1,51 @@
 #!/usr/bin/env python
-
 """
 Functions for processing BIOGRID interaction files
 
 Created: 25 November 2015
+
+@author: jyoung
 """
 
 import os.path
 
 
-def setup_filepaths():
+def setup_filepaths(organism):
     """Setup filepaths for BIOGRID files for various organisms."""
-    ScPath = os.path.join('..', 'data', 'BIOGRID-3.4.130-yeast-post2006.txt')
-    SpPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
-            'BIOGRID-ORGANISM-3.4.130.tab2', 
-            'BIOGRID-ORGANISM-Schizosaccharomyces_pombe_972h-3.4.130.tab2.txt')
-    DmPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
-            'BIOGRID-ORGANISM-3.4.130.tab2',
-            'BIOGRID-ORGANISM-Drosophila_melanogaster-3.4.130.tab2.txt')
-    HsPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
-            'BIOGRID-ORGANISM-3.4.130.tab2',
-            'BIOGRID-ORGANISM-Homo_sapiens-3.4.130.tab2.txt')
+    if organism == 'cerevisiae':
+        biogridPath = os.path.join('..', 'data', 
+                'BIOGRID-3.4.130-yeast-post2006.txt')
+    elif organism == 'pombe':
+        biogridPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
+                'BIOGRID-ORGANISM-3.4.130.tab2', 
+                'BIOGRID-ORGANISM-Schizosaccharomyces_pombe_972h-3.4.130.tab2'\
+                        '.txt')
+    elif organism == 'melanogaster':
+        biogridPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
+                'BIOGRID-ORGANISM-3.4.130.tab2', 
+                'BIOGRID-ORGANISM-Drosophila_melanogaster-3.4.130.tab2.txt')
+    else:  # organism == 'sapiens'
+        biogridPath = os.path.join('..', '..', 'DataDownload', 'BIOGRID', 
+                'BIOGRID-ORGANISM-3.4.130.tab2', 
+                'BIOGRID-ORGANISM-Homo_sapiens-3.4.130.tab2.txt')
 
-    org2path = {'cerevisiae': ScPath, 'pombe': SpPath, 'melanogaster': DmPath,
-            'sapiens': HsPath}
-
-    return org2path
-
-
-def get_biogrid_genes(organism, intactType):
-    """Return genes from BIOGRID for a given interaction type.
-    WARNING: GENE COLUMNS HARD-CODED"""
-    org2path = setup_filepaths()
-
-    intactGenes = set()
-    geneIntactFile = org2path[organism]
-    for line in open(geneIntactFile):
-        tokens = line.rstrip().split('\t')
-        if tokens[11] == intactType:
-            intactGenes.update(tokens[5:7])
-
-    return intactGenes
+    return biogridPath
 
 
-def get_interacting_genes(organism, intactType):
-    """Return set of gene pairs that are of given interaction type.
-    WARNING: GENE COLUMNS HARD-CODED"""
-    org2path = setup_filepaths()
-
+def get_interacting_genes(organism, intactType, colName):
+    """Return set of gene pairs that are of given interaction type"""
     intactSet = set()
-    geneIntactFile = org2path[organism]
-    for line in open(geneIntactFile):
+    
+    biogridFile = open(setup_filepaths(organism))
+    header = biogridFile.readline().rstrip().split('\t')
+    colNum = header.index(colName)
+    intactTypeCol = header.index('Experimental System')
+    
+    for line in biogridFile:
         tokens = line.rstrip().split('\t')
-        if tokens[11] == intactType:
-            intactSet.add(frozenset(tokens[5:7]))
+        if tokens[intactTypeCol] == intactType:
+            intactSet.add(frozenset(tokens[colNum:colNum + 2]))
+    biogridFile.close()
 
     return intactSet
 
